686d6386d897747a626e1e8a87008fd046084069
Devansh Jalota <djalota@stanford.edu>
2021-02-02 22:01:29 -0800

    adding path file

--- path_length.py
+++ path_length.py
@@ -2,9 +2,9 @@ import sys
 import pandas as pd
 import numpy as np
 
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print('Usage:')
-    print('  python3 {} [edge file] [flow file] [path file]'.format(sys.argv[0]))
+    print('  python3 {} [edge file] [flow file] [path file] [weights file]'.format(sys.argv[0]))
     sys.exit(0)
 
 #Read in the files
@@ -12,7 +12,7 @@ edges = pd.read_csv(sys.argv[1])
 edges_arr = np.array(edges)
 edges_arr = np.c_[edges_arr, np.array(range(len(edges)))]
 
-flow = pd.read_csv(sys.argv[2])
+flow = pd.read_csv(sys.argv[2], skiprows = 1)
 flow_arr = np.array(flow)
 
 paths_ = pd.read_csv(sys.argv[3], sep='delimiter', header=None)
@@ -20,6 +20,19 @@ paths_ = paths_.iloc[2:]
 paths_ = paths_[0].str.split(',', 2, expand = True)
 paths_arr = np.array(paths_)
 
+weights = pd.read_csv('Locations/SiouxFalls/result_so/weights.csv', skiprows = 1)
+weights_arr = np.array(weights)
+
+#Check if weight is at least 1/10th of the maximum weight
+small_arr = []
+for i in range(len(weights_arr)):
+    if weights_arr[i, 1] > max(weights_arr[:, 1])/2:
+        small_arr.append(i)
+#Find unique paths
+paths_arr[:, 0] = [int(i) for i in paths_arr[:, 0]]
+paths_arr = paths_arr[paths_arr[:, 0]>=min(small_arr)]
+paths_arr_new = paths_arr[:, 1:]
+paths_arr_new = np.vstack({tuple(row) for row in paths_arr_new})
 
 def find_edge2(tail_, head_):
     '''Finds edge index for given edge'''
@@ -66,22 +79,26 @@ def path_edge_finder2(edge_string):
     
     return find_edge2(int(simplify_string3[0]), int(simplify_string3[1]))
 
+#Find unique Paths
+#paths_arr_new = paths_arr[:, 1:]
+#paths_arr_new = np.vstack({tuple(row) for row in paths_arr_new})
+
 #Return the length of each path for each OD pair
 OD_dict_ = {}
 OD_path_lengths = {}
 
 for OD_pair in range(int(paths_arr[-1, 1])):
-    OD_arr = paths_arr[paths_arr[:, 1] == str(OD_pair)]
+    OD_arr = paths_arr_new[paths_arr_new[:, 0] == str(OD_pair)]
     all_paths_OD = []
     OD_tt_paths = []
-    for path_val in OD_arr[:, 2]:
+    for path_val in OD_arr[:, 1]:
         all_edges = path_val.split(',')
+        all_edges = [int(i) for i in all_edges]
         list_edges = []
         path_tt = 0
         for edge_values in all_edges:
-            edge_input = path_edge_finder2(edge_values)
-            list_edges.append(edge_input)
-            path_tt += link_latency(0.15, 4, int(flow_arr[edge_input, 3]),  edges_arr[edge_input, 3], x_sol[edge_input])
+            list_edges.append(edge_values)
+            path_tt += link_latency(0.15, 4, int(flow_arr[edge_values, 3]),  edges_arr[edge_values, 3], x_sol[edge_values])
 
         OD_tt_paths.append(path_tt)
 
@@ -92,7 +109,10 @@ for OD_pair in range(int(paths_arr[-1, 1])):
     OD_dict_[OD_pair] = all_paths_OD
     OD_path_lengths[OD_pair] = OD_tt_paths
 
-with open('OD_paths_tts.csv', 'w') as f:
+with open('OD_paths_tts2.csv', 'w') as f:
     for key in OD_path_lengths.keys():
         #print(key)
-        f.write("%s,%s\n"%(key,OD_path_lengths[key]))
+        str_val = str(OD_path_lengths[key])[1:-1]
+        
+        f.write("%s,%s\n"%(key,str_val))
+#        f.write("%s,%s\n"%(key,[i for i in OD_path_lengths[key]]))
